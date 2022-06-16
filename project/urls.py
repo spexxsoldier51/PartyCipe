73e7ce2b1146f17920065d5c7eb8bdbd1d9b24cf
spexxsoldier51 <75441752+spexxsoldier51@users.noreply.github.com>
2022-06-16 11:58:27 +0200

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from partycipe.views import *
 
 from welcome.views import index, health
 
