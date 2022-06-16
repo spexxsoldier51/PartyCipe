9da3b1788f5b82d832e23703b68d446a81829a04
spexxsoldier51 <75441752+spexxsoldier51@users.noreply.github.com>
2022-06-16 13:36:46 +0200

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -28,32 +28,30 @@ urlpatterns = [
     path('health/', health),
     path('admin/', admin.site.urls),
     
-    path('/partycipe/accounts/', include('django.contrib.auth.urls')),
-    path('/partycipe/signup/', SignUpView.as_view(), name='signup'),
+    path('partycipe/accounts/', include('django.contrib.auth.urls')),
+    path('partycipe/signup/', SignUpView.as_view(), name='signup'),
 
-    path('/partycipe/', Home),
+    path('partycipe/', Home),
 
-    path('/partycipe/party/join', JoinParty),
-    path('/partycipe/party/partipate', JoinParty),
-    path('/partycipe/party/create', create_party),
-    path('/partycipe/party/<int:id>', party_detail, name='party_detail'),
+    path('partycipe/party/join', JoinParty),
+    path('partycipe/party/partipate', JoinParty),
+    path('partycipe/party/create', create_party),
+    path('partycipe/party/<int:id>', party_detail, name='party_detail'),
 
 
 
-    path('/partycipe/participate/change/<int:id>', ChangeParticipate, name="ChangeParticipate"),
+    path('partycipe/participate/change/<int:id>', ChangeParticipate, name="ChangeParticipate"),
 
-    re_path(r'/partycipe/party\/(?P<signed_pk>(?:[0-9]+\/[A-Za-z0-9_=-]+))$', party_status, name='party-status'),
-    re_path(r'/partycipe/party\/(?P<signed_pk>(?:[0-9]+\/[A-Za-z0-9_=-]+))\/join$', party_join, name='party-join'),
+    re_path(r'partycipe/party\/(?P<signed_pk>(?:[0-9]+\/[A-Za-z0-9_=-]+))$', party_status, name='party-status'),
+    re_path(r'partycipe/party\/(?P<signed_pk>(?:[0-9]+\/[A-Za-z0-9_=-]+))\/join$', party_join, name='party-join'),
 
-    re_path(r'/partycipe/party\/(?P<signed_pk>(?:[0-9]+\/[A-Za-z0-9_=-]+))\/mail$', send_mail, name='send_mail'),
-    re_path(r'/partycipe/party\/(?P<signed_pk>(?:[0-9]+\/[A-Za-z0-9_=-]+))\/share$', share, name='share'),
+    re_path(r'partycipe/party\/(?P<signed_pk>(?:[0-9]+\/[A-Za-z0-9_=-]+))\/mail$', send_mail, name='send_mail'),
+    re_path(r'partycipe/party\/(?P<signed_pk>(?:[0-9]+\/[A-Za-z0-9_=-]+))\/share$', share, name='share'),
 
-    re_path(r'/partycipe/party\/(?P<signed_pk>(?:[0-9]+\/[A-Za-z0-9_=-]+))\/add_cocktail\/(?P<id>(?:[0-9]+))', add_cocktail, name='add_cocktail'),
-    re_path(r'/partycipe/party\/(?P<signed_pk>(?:[0-9]+\/[A-Za-z0-9_=-]+))\/delete_cocktail\/(?P<id>(?:[0-9]+))', delete_cocktail, name='delete_cocktail'),
+    re_path(r'partycipe/party\/(?P<signed_pk>(?:[0-9]+\/[A-Za-z0-9_=-]+))\/add_cocktail\/(?P<id>(?:[0-9]+))', add_cocktail, name='add_cocktail'),
+    re_path(r'partycipe/party\/(?P<signed_pk>(?:[0-9]+\/[A-Za-z0-9_=-]+))\/delete_cocktail\/(?P<id>(?:[0-9]+))', delete_cocktail, name='delete_cocktail'),
 
-    path('/partycipe/change-password/', auth_views.PasswordChangeView.as_view(template_name='registration/change_password.html',success_url = '/'),name='change_password'),
-
-    
+    path('partycipe/change-password/', auth_views.PasswordChangeView.as_view(template_name='registration/change_password.html',success_url = '/'),name='change_password'),
 ]
 
 if settings.DEBUG:
