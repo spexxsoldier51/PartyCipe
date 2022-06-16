a0d4fbd3c12d78fb0b44dbf96a9b75dd96791a3d
spexxsoldier51 <75441752+spexxsoldier51@users.noreply.github.com>
2022-06-16 13:43:34 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -158,3 +158,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 INTERNAL_IPS = ['127.0.0.1']
+
+LOGIN_URL = '/partycipe/accounts/login'
